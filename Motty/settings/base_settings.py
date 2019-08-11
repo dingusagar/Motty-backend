cf3537c2c2deb2e039edab87c551c4d4295330de
dingusagar <dingusagar@gmail.com>
2019-08-12 00:45:17 +0530

    allowed hosts

--- Motty/settings/base_settings.py
+++ Motty/settings/base_settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '0dxfuhrb2x&dv)d451(*l16fzlrvzc=+pgm56hg8arow25&wnv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
